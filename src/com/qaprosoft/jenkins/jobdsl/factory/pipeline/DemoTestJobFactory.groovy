ee29856a41a795e779c8ca7259d1a057ae741017
Sandino Sanchez <43247306+sandinosanchez@users.noreply.github.com>
2020-06-26 15:46:43 -0300

    Update DemoTestJobFactory.groovy

--- src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/DemoTestJobFactory.groovy
+++ src/com/qaprosoft/jenkins/jobdsl/factory/pipeline/DemoTestJobFactory.groovy
@@ -3,7 +3,6 @@ package com.qaprosoft.jenkins.jobdsl.factory.pipeline
 import groovy.transform.InheritConstructors
 import com.qaprosoft.jenkins.jobdsl.factory.pipeline.TestJobFactory
 
-
 @InheritConstructors
 class DemoTestJobFactory extends TestJobFactory {
 
